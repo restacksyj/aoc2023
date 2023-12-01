e5f46d84e0e81d3a0b15030ddaf91fda2cf496a5
YASH JAJOO <yashjajoo@192.168.1.13>
2023-12-02 03:54:07 +0530

    removed unused file

--- day1/one.js
+++
@@ -1,7 +0,0 @@
-const inputData = require('./one_data.js')
-
-//splitting input on \n since it has newlines
-const arr = inputData.split("\n");
-// deleting the first empty "" from the input
-arr.shift()
-
